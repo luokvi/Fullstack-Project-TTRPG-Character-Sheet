d8674a68e457b14cd726eb5ba1445d76caef3838
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-01-18 19:29:16 +0200

    Update to using useDispatch and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { initializeCharacters } from './reducers/characterReducer'
 import Characters from './Characters'
 
 const App = () => {
-  
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeCharacters())
+  }, [dispatch])
+
   return (
     <div>
       <h2>TTRPG Character</h2>
