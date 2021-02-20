a1bb2f9dffa371425ae4b385a7c88ed6a80ae836
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2021-02-20 20:32:46 +0200

    Update to add equipment

--- src/Character.js
+++ src/Character.js
@@ -4,12 +4,35 @@ import { useParams } from 'react-router-dom'
 const Character = ({ charas }) => {
     const id = useParams().id
     const chara = charas.find(c => c.id === Number(id))
+
+    if (!chara){
+        return(
+            <div>
+                <p>loading...</p>
+            </div>
+        )
+    }
+
+    const stat = (character, weapon, armor) => {
+        if (!weapon && !armor){
+            return character
+        }
+
+        let total = character
+        if (weapon) total += weapon
+        if(armor) total += armor
+
+        return total
+    }
+
+    const weapon = chara.equipment.weapon
+    const armor = chara.equipment.armor
     
     return (
         <div>
             <h2>{chara.name},</h2>
             <h3>{chara.class}, level {chara.level}</h3>
-            <p>HP: {chara.currentHitpoints}/{chara.hitpoints}</p>
+            <p>HP: {chara.currentHitpoints}/{stat(chara.hitpoints, weapon.hitpoints, armor.hitpoints)}</p>
             <table>
                 <tbody>
                     <tr>
@@ -20,11 +43,11 @@ const Character = ({ charas }) => {
                         <th>lck</th>
                     </tr>
                     <tr>
-                        <td>{chara.strength}</td>
-                        <td>{chara.dexterity}</td>
-                        <td>{chara.vitality}</td>
-                        <td>{chara.magic}</td>
-                        <td>{chara.luck}</td>
+                        <td>{stat(chara.strength, weapon.strength, armor.strength)}</td>
+                        <td>{stat(chara.dexterity, weapon.dexterity, armor.dexterity)}</td>
+                        <td>{stat(chara.vitality, weapon.vitality, armor.vitality)}</td>
+                        <td>{stat(chara.magic, weapon.magic, armor.magic)}</td>
+                        <td>{stat(chara.luck, weapon.luck, armor.luck)}</td>
                     </tr>
                 </tbody>
             </table>
@@ -38,13 +61,17 @@ const Character = ({ charas }) => {
                         <th>magic ar</th>
                     </tr>
                     <tr>
-                        <td>{chara.attack}</td>
-                        <td>{chara.armor}</td>
-                        <td>{chara.magicAttack}</td>
-                        <td>{chara.magicArmor}</td>
+                        <td>{stat(chara.attack, weapon.attack, armor.attack)}</td>
+                        <td>{stat(chara.armor, weapon.armor, armor.armor)}</td>
+                        <td>{stat(chara.magicAttack, weapon.magicAttack, armor.magicAttack)}</td>
+                        <td>{stat(chara.magicArmor, weapon.magicArmor, armor.magicArmor)}</td>
                     </tr>
                 </tbody>
             </table>
+
+            <p><b>Equipment</b></p>
+            <p>Weapon: {weapon.name}</p>
+            <p>Armor: {armor.name}</p>
         </div>
     )
 }
